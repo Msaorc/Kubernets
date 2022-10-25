e4441ba99bb8e079b7f6753e22dd9205eea6d927
Marcos Augusto <msaorc@hotmail.com>
2022-10-25 11:07:51 -0300

    Criação de cluster Kubernets sem publicação em produção

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "net/http"
+
+func main() {
+	http.HandleFunc("/", hello)
+	http.ListenAndServe(":9090", nil)
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Welcome to the api in go with Kubernets"))
+}
